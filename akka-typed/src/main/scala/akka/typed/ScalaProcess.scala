24a45fb7277a1f24976010021893658145a09f76
Roland Kuhn <rk@rkuhn.info>
2016-12-22 23:36:03 +0100

    fix Operation variance: it did not work

--- akka-typed/src/main/scala/akka/typed/ScalaProcess.scala
+++ akka-typed/src/main/scala/akka/typed/ScalaProcess.scala
@@ -6,8 +6,16 @@ package akka.typed
 import scala.concurrent.duration._
 import akka.actor.Cancellable
 
-private[typed] trait ScalaProcess {
+object ScalaProcess {
   import ScalaProcess._
+
+  class RetriesExceeded extends RuntimeException
+
+  import language.implicitConversions
+  /**
+   * This implicit expresses that operations that do not use their input channel can be used in any context.
+   */
+  private implicit def nothingIsSomething[T, U](op: Operation[Nothing, T]): Operation[U, T] = op.asInstanceOf[Operation[U, T]]
 
   /*
    * Terminology:
@@ -16,63 +24,57 @@ private[typed] trait ScalaProcess {
    *  - an Operation is a step that a Process takes and that produces a value
    *  - Processes are concurrent, but not distributed: all failures stop the entire Actor
    *  - each Process has its own identity (due to ActorRef), and the Actor has its own
-   *    identity (an ActorRef[ActorCmd[_]]); Self is the Process’ identity, ActorSelf is the Actor’s
+   *    identity (an ActorRef[ActorCmd[_]]); processSelf is the Process’ identity, actorSelf is the Actor’s
    *  - timeout means failure
+   *  - every Actor has a KV store for state
+   *      - querying by key in TypedMultiMap (using a single element per slot)
+   *      - updating is an Operation taking an Event and an implicit (Event, State) => State
+   *      - persistence can then be plugged in transparently
+   *      - recovery means acquiring state initially (which might trigger internal replay)
    */
 
   // Helper to make map behave like flatMap when needed
-  trait MapAdapter[Out, Mapped] {
-    def lift[O, Self](f: O ⇒ Out): O ⇒ Operation[Self, Mapped]
+  trait MapAdapter[Self, Out, Mapped] {
+    def lift[O](f: O ⇒ Out): O ⇒ Operation[Self, Mapped]
   }
   object MapAdapter extends MapAdapterLow {
-    implicit def mapAdapterOperation[S, M]: MapAdapter[Operation[S, M], M] =
-      new MapAdapter[Operation[S, M], M] {
-        override def lift[O, Self](f: O ⇒ Operation[S, M]): O ⇒ Operation[Self, M] = f.asInstanceOf[O => Operation[Self, M]]
+    implicit def mapAdapterOperation[Self, M]: MapAdapter[Self, Operation[Self, M], M] =
+      new MapAdapter[Self, Operation[Self, M], M] {
+        override def lift[O](f: O ⇒ Operation[Self, M]): O ⇒ Operation[Self, M] = f
       }
   }
   trait MapAdapterLow {
-    implicit def mapAdapterAny[Out]: MapAdapter[Out, Out] =
-      new MapAdapter[Out, Out] {
-        override def lift[O, Self](f: O ⇒ Out): O ⇒ Operation[Self, Out] = o ⇒ Return(f(o))
+    implicit def mapAdapterAny[Self, Out]: MapAdapter[Self, Out, Out] =
+      new MapAdapter[Self, Out, Out] {
+        override def lift[O](f: O ⇒ Out): O ⇒ Operation[Self, Out] = o ⇒ Return(f(o))
       }
   }
 
-  def res[T](value: T): Operation[T, T] = OpDSL[T] { implicit opDSL ⇒
-    for {
-      self ← processSelf
-      _ ← schedule(null, value, self)
-    } yield read
-  }
-
-  sealed trait Operation[-S, +Out] {
-    def flatMap[U <: S, T](f: Out ⇒ Operation[U, T]): Operation[U, T] = FlatMap(this, f)
-    def map[U <: S, T, Mapped](f: Out ⇒ T)(implicit ev: MapAdapter[T, Mapped]): Operation[U, Mapped] = flatMap(ev.lift(f))
+  sealed trait Operation[S, +Out] {
+    def flatMap[T](f: Out ⇒ Operation[S, T]): Operation[S, T] = FlatMap(this, f)
+    def map[T, Mapped](f: Out ⇒ T)(implicit ev: MapAdapter[S, T, Mapped]): Operation[S, Mapped] = flatMap(ev.lift(f))
     def foreach(f: Out ⇒ Unit): Operation[S, Unit] = flatMap(o ⇒ Return(f(o)))
     def filter(p: Out ⇒ Boolean): Operation[S, Out] = flatMap(o ⇒ if (p(o)) Return(o) else ShortCircuit)
     def withFilter(p: Out ⇒ Boolean): Operation[S, Out] = flatMap(o ⇒ if (p(o)) Return(o) else ShortCircuit)
   }
 
   private[this] case class FlatMap[S, Out1, Out2](first: Operation[S, Out1], then: Out1 ⇒ Operation[S, Out2]) extends Operation[S, Out2]
-  private[this] case object ShortCircuit extends Operation[Any, Nothing] {
-    override def flatMap[U, T](f: Nothing ⇒ Operation[U, T]): ShortCircuit.type = this
+  private[this] case object ShortCircuit extends Operation[Nothing, Nothing] {
+    override def flatMap[T](f: Nothing ⇒ Operation[Nothing, T]): Operation[Nothing, T] = this
   }
 
   // these are the private objects, to be obtained via the DSL
-  private case object System extends Operation[Any, ActorSystem[Nothing]]
-  private case object Read extends Operation[Any, Nothing]
-  private case object Self extends Operation[Any, ActorRef[Any]]
-  private case object ActorSelf extends Operation[Any, ActorRef[ActorCmd[Nothing]]]
-  private case class Return[T](value: T) extends Operation[Any, T]
-  private case class Call[T](process: Process[Nothing, T]) extends Operation[Any, T]
-  private case class Fork[S](process: Process[S, Any]) extends Operation[Any, SubActor[S]]
-  private case class Spawn[S](process: Process[S, Any]) extends Operation[Any, ActorRef[ActorCmd[S]]]
-  private case class Schedule[T](delay: FiniteDuration, msg: T, target: ActorRef[T]) extends Operation[Any, Cancellable]
+  private case object System extends Operation[Nothing, ActorSystem[Nothing]]
+  private case object Read extends Operation[Nothing, Nothing]
+  private case object Self extends Operation[Nothing, ActorRef[Any]]
+  private case object ActorSelf extends Operation[Nothing, ActorRef[ActorCmd[Nothing]]]
+  private case class Return[T](value: T) extends Operation[Nothing, T]
+  private case class Call[S, T](process: Process[S, T]) extends Operation[Nothing, T]
+  private case class Fork[S](process: Process[S, Any]) extends Operation[Nothing, SubActor[S]]
+  private case class Spawn[S](process: Process[S, Any]) extends Operation[Nothing, ActorRef[ActorCmd[S]]]
+  private case class Schedule[T](delay: FiniteDuration, msg: T, target: ActorRef[T]) extends Operation[Nothing, Cancellable]
 
-  case class Process[-S, +Out](name: String, timeout: Duration, operation: Operation[S, Out])
-  object ProcessN {
-    def apply[Out](name: String, timeout: Duration, operation: Operation[Nothing, Out]): Process[Nothing, Out] =
-      Process[Nothing, Out](name, timeout, operation)
-  }
+  case class Process[S, +Out](name: String, timeout: Duration, operation: Operation[S, Out])
 
   /*
    * The core operations: keep these minimal!
@@ -82,12 +84,10 @@ private[typed] trait ScalaProcess {
   def processSelf(implicit opDSL: OpDSL): Operation[opDSL.Self, ActorRef[opDSL.Self]] = Self
   def actorSelf(implicit opDSL: OpDSL): Operation[opDSL.Self, ActorRef[ActorCmd[Nothing]]] = ActorSelf
   def unit[U](value: U)(implicit opDSL: OpDSL): Operation[opDSL.Self, U] = Return(value)
-  def call[Out](process: Process[Nothing, Out])(implicit opDSL: OpDSL): Operation[opDSL.Self, Out] = Call(process)
+  def call[Self, Out](process: Process[Self, Out])(implicit opDSL: OpDSL): Operation[opDSL.Self, Out] = Call(process)
   def nextStep[T] = OpDSL.nextStep.asInstanceOf[OpDSL.NextStep[T]]
   def fork[Self](process: Process[Self, Any])(implicit opDSL: OpDSL): Operation[opDSL.Self, SubActor[Self]] = Fork(process)
-  def forkN(process: Process[Nothing, Any])(implicit opDSL: OpDSL): Operation[opDSL.Self, SubActor[Nothing]] = Fork[Nothing](process)
   def spawn[Self](process: Process[Self, Any])(implicit opDSL: OpDSL): Operation[opDSL.Self, ActorRef[ActorCmd[Self]]] = Spawn(process)
-  def spawnN(process: Process[Nothing, Any])(implicit opDSL: OpDSL): Operation[opDSL.Self, ActorRef[ActorCmd[Nothing]]] = Spawn[Nothing](process)
   def schedule[T](delay: FiniteDuration, msg: T, target: ActorRef[T])(implicit opDSL: OpDSL): Operation[opDSL.Self, Cancellable] = Schedule(delay, msg, target)
 
   /*
@@ -99,15 +99,6 @@ private[typed] trait ScalaProcess {
 
   object OpDSL {
     def apply[T]: Next[T] = next.asInstanceOf[Next[T]]
-
-    /*
-     * This implicit is picked up if nothing else is available. It is unproblematic
-     * because the resulting input operations are unconstrained and therefore fit
-     * into any context.
-     */
-    implicit object noReads extends OpDSL {
-      type Self = Any
-    }
 
     trait Next[T] {
       def apply[U](body: OpDSL { type Self = T } ⇒ Operation[T, U]): Operation[T, U] = body(null)
@@ -124,22 +115,22 @@ private[typed] trait ScalaProcess {
   /*
    * Derived operations
    */
-  def firstOf[T](timeout: Duration, processes: Operation[Nothing, T]*): Operation[Any, T] = {
+  def firstOf[T](timeout: Duration, processes: Operation[_, T]*)(implicit opDSL: OpDSL): Operation[opDSL.Self, T] = {
     def forkAll(self: ActorRef[T], index: Int = 0,
-                p: List[Operation[Nothing, T]] = processes.toList,
+                p: List[Operation[_, T]] = processes.toList,
                 acc: List[SubActor[Nothing]] = Nil)(implicit opDSL: OpDSL { type Self = T }): Operation[T, List[SubActor[Nothing]]] =
       p match {
         case Nil     ⇒ unit(acc)
-        case x :: xs ⇒ forkN(ProcessN(index.toString, timeout, x)).map(sub ⇒ forkAll(self, index + 1, xs, sub :: acc))
+        case x :: xs ⇒ fork(Process(index.toString, timeout, x)).map(sub ⇒ forkAll(self, index + 1, xs, sub :: acc))
       }
-    call(ProcessN("firstOf", timeout, OpDSL[T] { implicit opDSL ⇒
+    call(Process("firstOf", timeout, OpDSL[T] { implicit opDSL ⇒
       for {
         self ← processSelf
         subs ← forkAll(self)
         value ← read
       } yield {
         subs.foreach(_.cancel())
-        unit(value)
+        value
       }
     }))
   }
@@ -152,15 +143,15 @@ private[typed] trait ScalaProcess {
       } yield read
     }
 
-  def forkAndCancel[T](timeout: FiniteDuration, process: Process[T, Any]): Operation[Any, SubActor[T]] =
+  def forkAndCancel[T](timeout: FiniteDuration, process: Process[T, Any])(implicit opDSL: OpDSL): Operation[opDSL.Self, SubActor[T]] =
     for {
       sub ← fork(process)
       _ ← fork(Process("cancelAfter", Duration.Inf, delay(timeout, ()).foreach(_ ⇒ sub.cancel())))
     } yield unit(sub)
 
-  def retry[T](timeout: FiniteDuration, retries: Int, ops: Operation[Nothing, T]): Operation[Any, T] = {
-    call(ProcessN("retry", Duration.Inf,
-      firstOf(Duration.Inf, ops.map(res ⇒ unit(Some(res))), delay(timeout, None))
+  def retry[S, T](timeout: FiniteDuration, retries: Int, ops: Operation[S, T])(implicit opDSL: OpDSL): Operation[opDSL.Self, T] = {
+    call(Process("retry", Duration.Inf,
+      firstOf(Duration.Inf, ops.map(Some(_)), delay(timeout, None))
     ))
       .map {
         case Some(res)           ⇒ unit(res)
@@ -177,14 +168,8 @@ private[typed] trait ScalaProcess {
   sealed trait ActorCmd[+T]
   case class MainCmd[T](cmd: T) extends ActorCmd[T]
 
-  trait SubActor[T] {
+  trait SubActor[-T] {
     def ref: ActorRef[T]
     def cancel(): Unit
   }
-}
-
-object ScalaProcess extends ScalaProcess {
-  // so that this high-level DSL can be imported separately
-
-  class RetriesExceeded extends RuntimeException
 }
